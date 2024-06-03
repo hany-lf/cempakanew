b7e016afbe2d48c145b68228981aa39908923dac
hany-lf <haniyya.ulfah@ifca.co.id>
2024-06-03 14:20:25 +0700

    3/6, xcode runing apk

--- app/components/StoreAsync.js
+++ app/components/StoreAsync.js
@@ -1,4 +1,4 @@
-import AsyncStorage from '@react-native-community/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Actions } from "react-native-router-flux";
 
 export const _storeData = async (name,data) =>{
